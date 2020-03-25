eada80a7ad0b1ef5d4baa499f7b3aebb7d72adb5
bnding <bnding@gmail.com>
2020-03-25 16:47:22 -0400

    moving over code from project 1

--- client.py
+++ client.py
@@ -1,1 +1,99 @@
+import socket as csock
+import argparse
+import time
 
+DOMAIN_NAMES = []
+
+def read_domain_names():
+    address_file = open("PROJI-HNS.txt", "r")
+    arr = address_file.readlines()
+    return arr
+
+def lsConnect(lsPort, lsHostName):
+
+    # Establishing Connection with RS
+    # TODO: Server lookup addr. Check if RServer has the data or TServer does
+    try:
+        cs = csock.socket(csock.AF_INET, csock.SOCK_STREAM)
+        print("[C]: LS Client Socket created")
+    except csock.error as err:
+        print('{} \n'.format("socket open error ",err))
+    
+    # addr = csock.gethostbyname(csock.gethostname())
+    addr = lsHostName
+
+    cs.connect((addr, lsPort))
+
+    return cs
+
+def tsConnect(tsHostName,tsPort ):
+    # Establishing connection with TS
+    # Commented out to test code between client and RS only
+    # TODO: if condition here if RServer returns "TSHostName - NS"
+    try:
+        tcs = csock.socket(csock.AF_INET, csock.SOCK_STREAM)
+        print("[C]: TS Client Socket created")
+    except csock.error as err:
+        print('{} \n'.format("socket open error ",err))
+    addr = tsHostName
+    tcs.connect((addr, tsPort))
+    return tcs
+
+parser = argparse.ArgumentParser()
+
+# Default arg is set to string type=int overrides
+parser.add_argument("lsHostName", help="input host data")
+parser.add_argument("lsListenPort", type=int, help="input ls port number")
+parser.add_argument("tsListenPort", type=int, help="input ts port number")
+args = parser.parse_args()
+print("[C]: host file name: " + args.lsHostName)
+print("[C]: ls socket: " + str(args.lsListenPort))
+print("[C]: ts socket: " + str(args.tsListenPort))
+
+#connect to LS server
+lsSocket = lsConnect(args.lsListenPort, args.lsHostName)
+
+# get host name for TS by sending invalid query 
+garbage_content = "garbagevalue"
+garbage_content =  str("{:<200}".format(garbage_content))
+lsSocket.send(garbage_content.encode('utf-8'))
+LSresponse = lsSocket.recv(200).decode('utf-8')
+print("[C]: Response received::  "+LSresponse)
+tsHost_arr = LSresponse.split("-")
+tsHostName = tsHost_arr[0].strip()
+if tsHostName == "localhost":
+    tsHostName = args.lsHostName
+
+# connect to TS server
+tsSocket = tsConnect(tsHostName, args.tsListenPort)
+
+address_file = open("PROJI-HNS.txt", "r")
+output_file = open("RESOLVED.txt", "w")
+
+
+fileInfo = ""
+for line in address_file:
+    line =  str("{:<200}".format(line))
+    print("[C]: Response sent::  "+line)
+    lsSocket.send(line.encode('utf-8'))
+    LSresponse = lsSocket.recv(200).decode('utf-8')
+    print("[C]: Response recieved::  "+LSresponse)
+    if "NS" in LSresponse:
+        print("[C]: Sending to TS ...  ")
+        print("[C]: Response sent to TS::  "+line)
+        tsSocket.send(line.encode('utf-8'))
+        TSresponse = tsSocket.recv(200).decode('utf-8')
+        print("[C]: Response received from TS::  "+TSresponse)
+        fileInfo = fileInfo + TSresponse.strip() + "\n" 
+        # output_file.write(TSresponse.strip()+"\n")
+    else:
+        fileInfo = fileInfo + LSresponse.strip() + "\n"
+        # output_file.write(LSresponse.strip()+"\n")
+fileInfo = fileInfo[:-1]
+output_file.write(fileInfo)
+
+lsSocket.send("DONE".encode('utf-8'))
+lsSocket.close()
+tsSocket.send("DONE".encode('utf-8'))
+tsSocket.close()
+exit()
--- ls.py
+++ ls.py
@@ -1,1 +1,66 @@
+import socket
+import argparse
+import time
+DNS_TABLE = []
+HEADER_VALUES = ["hostname", "ip", "flag"]
+def set_up_dns_table():
+    f = open("PROJI-DNSRS.txt", "r")
+    for line in f:
+        line = line.strip("\n") # remove extra new lines
+        line_info = line.split(" ") # split line by spaces
 
+        # create dictionary of values and add to array 
+        combined_info = zip(HEADER_VALUES, line_info) 
+        DNS_TABLE.append(dict(combined_info))
+
+    f.close()
+
+def find_ip(queried_host):
+    # check if the IP is in the table 
+    for row in DNS_TABLE:
+        if row["hostname"].lower() == queried_host.lower():  # case insensitive
+            return row["hostname"] + " " + row["ip"] + " " + row["flag"]+ "\n"
+    
+    # if not in the table find the NS host name and return 
+    for row in DNS_TABLE:
+        if row["flag"] == "NS":
+            return row["hostname"] +" - "+ row["flag"]+ "\n"
+    
+def clientConnect(port):
+    try:
+        ss=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[LS]: Server socket created")
+    except socket.error as err:
+        print('{} \n'.format("socket open error ",err))
+
+    server_binding=('',port)
+    ss.bind(server_binding)
+    ss.listen(1)
+    host=socket.gethostname()
+    print("[LS]: Server host name is: " + host)
+    localhost_ip=(socket.gethostbyname(host))
+    print("[LS]: Server IP address is  " + localhost_ip)
+    csockid,addr=ss.accept()
+    print ("[LS]: Got a connection request from a client at " + addr[0] + " " + str(addr[1]))
+
+    while 1:
+        request = csockid.recv(200).decode('utf-8')
+        print("[LS]: Message received:: " + request)
+        if request == "DONE": 
+            break
+        response = find_ip(request.strip())
+        response =  "{:<200}".format(response)
+        print("[LS]: Response Sent:: " + response)
+        csockid.send(response.encode('utf-8'))
+
+    ss.close()
+    exit()
+
+parser = argparse.ArgumentParser()
+
+# Default arg is set to string type=int overrides
+parser.add_argument("lsListenPort", type=int, help="input a port number")
+args = parser.parse_args()
+print("[LS]: Listening on port " + str(args.lsListenPort) + "...")
+set_up_dns_table()
+clientConnect(args.lsListenPort)
